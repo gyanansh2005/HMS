a7d3d1f0a2b0146419e09ebcdbf4d9b93e9f21f6
gyanansh2005 <gyanansh.kumar@gmail.com>
2025-04-09 00:49:06 +0530

    django

--- HMS Django/HMS terminal/myenv/Scripts/activate.bat
+++ HMS Django/HMS terminal/myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\MSI-LP\Desktop\Hms\myenv"
+set "VIRTUAL_ENV=C:\Users\gyana\Desktop\HMSmain\HMS\HMS Django\Hms terminal\myenv"
 
 if not defined PROMPT set PROMPT=$P$G
 
